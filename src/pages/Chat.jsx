75163fd73fd7f12f772981c7efbc12635488fd0a
thaonguyen <thaonguyenlephuong292@gmail.com>
2022-11-24 18:31:52 +0700

    undo chat

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,12 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import React from "react";
 
-//import { CometChatUI } from "../cometchat-pro-react-ui-kit-master/cometchat-pro-react-ui-kit/CometChatWorkspace/src";
+import { CometChatUI } from "../cometchat-pro-react-ui-kit-master/cometchat-pro-react-ui-kit/CometChatWorkspace/src";
 // import { CometChatUI } from "../../cometchat-pro-react-ui-kit/";
 const CometChat = () => {
   return (
     <Box style={{ width: "98vw", height: "95vh", backgroundColor: "white" }}>
-      {/* <CometChatUI /> */}
+      <CometChatUI />
     </Box>
   );
 };
